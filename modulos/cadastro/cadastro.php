096c665ee0b9a761b9e6f939c4b10e143766f58b
maarceloe <marceloverardino@hotmail.com>
2024-04-18 21:15:58 -0300

    att comeco

--- modulos/cadastro/cadastro.php
+++ modulos/cadastro/cadastro.php
@@ -3,14 +3,17 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../assets/css/style.css">
         <title>Cadastro de Usuário</title>
     </head>
     <body>
         <form action="login.php" method="POST" name="cadastro">
 
-            <h1>CORNO DO CARALHO</h1>
+            <h1>Cadastro de Usuário</h1>
+            <input type="text" value="Nome Completo">
+            <input type="email" value="E-mail">
             
-            <input type="submit" value="Enviar">
+            <input type="submit" value="Cadastrar">
         </form>
     </body>
 </html>
