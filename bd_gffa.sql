6913046cf0d5e2d8cd843dad774b0b6f252f4b1b
ar7utz <pereira.artur@hotmail.com>
2024-04-24 16:42:41 -0300

    sql correto e att

--- bd_gffa.sql
+++ bd_gffa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 18-Abr-2024 às 19:19
+-- Tempo de geração: 24-Abr-2024 às 21:41
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
 
@@ -36,6 +36,36 @@ CREATE TABLE `login` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `transacoes`
+--
+
+CREATE TABLE `transacoes` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(260) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `data` date NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `transacoes`
+--
+
+INSERT INTO `transacoes` (`id`, `descricao`, `valor`, `data`, `usuario_id`) VALUES
+(1, 'a', 12.00, '2024-04-23', 0),
+(2, 'a', 12.00, '2024-04-23', 0),
+(3, 'a', 12.00, '2024-04-23', 0),
+(4, 'toma', 1.00, '2024-04-23', 0),
+(5, 'toma', 1.00, '2024-04-23', 0),
+(6, 'toma', 1.00, '2024-04-23', 0),
+(7, '2', 1.00, '2024-04-23', 0),
+(8, 'testenfega', -20.00, '2024-04-23', 0),
+(9, 'teste', -10.00, '2024-04-23', 0),
+(10, '23', 2.00, '2024-04-23', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `user`
 --
 
@@ -47,6 +77,18 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Extraindo dados da tabela `user`
+--
+
+INSERT INTO `user` (`id`, `nome`, `email`, `senha`) VALUES
+(1, 'marcelo', 'marcelo.eduardo@hotmail.com', '123'),
+(2, '', '123@asd.com', '123'),
+(3, '', '123@asd.com', '123'),
+(4, '', '123@asd.com', '123'),
+(5, '', '123@asd.com', '123'),
+(6, 'teste2222', 'pereira.artur@hotmail.com', '123');
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -55,6 +97,13 @@ CREATE TABLE `user` (
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `transacoes`
+--
+ALTER TABLE `transacoes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Índices para tabela `user`
@@ -73,10 +122,16 @@ ALTER TABLE `login`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `transacoes`
+--
+ALTER TABLE `transacoes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT de tabela `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
