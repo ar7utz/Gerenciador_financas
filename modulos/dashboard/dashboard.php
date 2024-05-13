7ffc2a150a83694774379dbc25fcf020adf824c5
ar7utz <pereira.artur@hotmail.com>
2024-05-13 00:11:06 -0300

    mudei o title

--- modulos/dashboard/dashboard.php
+++ modulos/dashboard/dashboard.php
@@ -19,8 +19,7 @@ include ('../../assets/bd/conexao.php');
       href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,400;0,700;1,100;1,400;1,700&display=swap"
       rel="stylesheet"
     />
-
-    <title>Gerenciamento de Finanças</title>
+    <title>Dashboard</title>
   </head>
   <body>
     <header>
